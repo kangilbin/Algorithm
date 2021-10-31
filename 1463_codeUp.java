e4e49849adeb6e2fa6c95f54a7ea6ec09b60f107
kangilbin <kib3388@naver.com>
2021-10-31 20:29:11 +0900

    1463 코드업 문제

---
+++ 1463_codeUp.java
@@ -0,0 +1,23 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+   	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int N =  Integer.parseInt(br.readLine()); //int 형으로 입력
+	        int arr[][] = new int[N][N];
+	        int num = N; // 더하는 값
+	        int cnt = N; //1열 값
+	        for(int i=0; i < N; i++){
+	            cnt = num;
+	            for(int j=0; j<N; j++){
+	                arr[i][j] = cnt;
+	                System.out.print(arr[i][j]+" ");
+	                cnt += N;
+	            }
+	            num--;
+	            System.out.println("");
+	    }
+	}
+}
