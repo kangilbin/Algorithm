ca49f501ab116a683aceb61c18a382a7745bd8a0
kangilbin <kib3388@naver.com>
2021-11-02 21:09:52 +0900

    1172 코드업

---
+++ 1172_codeUp.java
@@ -0,0 +1,26 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+public class Main{
+
+   	public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int arr[] = new int[3];
+
+		for (int i = 0; i < 3; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[i] = num;
+		}
+
+		Arrays.sort(arr);
+
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+   	}
+}
