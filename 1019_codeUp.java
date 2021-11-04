e8b4ac3dd7b836a6f43504f755ad90a14596870a
kangilbin <kib3388@naver.com>
2021-11-04 23:53:33 +0900

    코드업

---
+++ 1019_codeUp.java
@@ -0,0 +1,26 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Main{
+
+   	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String[] arr = str.split("[.]");
+		String result = "";
+		if(arr[0].length() < 5) {
+			for(int i=arr[0].length(); i<4; i++) {
+				arr[0] = "0"+arr[0];
+			}
+			result = arr[0];
+		}
+		
+		for(int i=1; i<3; i++) {
+			if(Integer.parseInt(arr[i]) < 10) {
+				arr[i] = "0" + Integer.parseInt(arr[i]);
+			} 
+			result += "."+arr[i];
+	    }
+		System.out.println(result);
+	}
+}
