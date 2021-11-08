ce44e7719fabc0e3aa178c2906dbbac934ed4b03
kangilbin <kib3388@naver.com>
2021-11-09 00:14:32 +0900

    코드업

---
+++ 1094_codeUp.java
@@ -0,0 +1,22 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+
+   	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 int N =  Integer.parseInt(br.readLine()); 
+		 int arr[] = new int[N];
+		 
+		 StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+		 for(int i=0; i<N; i++) {
+			 int num =  Integer.parseInt(st.nextToken()); 
+			 arr[i] = num;
+		 }
+		 
+		 for(int i=N-1; 0<=i; i--) {
+			 System.out.print(arr[i]+" ");
+		 }
+   	}
+}
