95da2b1d16428788548dad1948e1b82d1ce21f6d
kangilbin <kib3388@naver.com>
2021-11-09 23:22:23 +0900

    코드업

---
+++ 1085_codeUp.java
@@ -0,0 +1,17 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+   	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		long h =  Integer.parseInt(st.nextToken()); 
+		long b =  Integer.parseInt(st.nextToken()); 
+		long c =  Integer.parseInt(st.nextToken()); 
+		long s =  Integer.parseInt(st.nextToken()); 
+		double num = h*b*c*s;
+		double result = num/8/1024/1024; 
+		System.out.println(Math.round(result*10)/10.0 + " MB");
+   	}
+}
