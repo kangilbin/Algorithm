f87f2891d4c23b1db9062961ac0cb376746483cd
kangilbin <kib3388@naver.com>
2021-11-30 00:09:39 +0900

    프로그래머스

--- 프로그래머스/기능개발.java
+++ 프로그래머스/기능개발.java
@@ -1,45 +1,38 @@
-package test;
-
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-public class Main{
-   	public static void main(String[] args) throws IOException {
-		int participant[] = {93, 30, 55};	
-		int completion[] = {1, 30, 5};	
-		solution(participant, completion);
-
-   	}
-    public static int[] solution(int[] progresses, int[] speeds) {
-    	Queue<Integer> que = new LinkedList<>();
-    	int time;
-    	for(int i=0; i<progresses.length; i++) {
-    		int cnt=1; 
-    		time = 100 - progresses[i];
-    		while(time > 0) {
-    			time -= speeds[i];
-    			cnt++;
-    		}
-    		que.add(cnt-1);
-    	}
-    	int[] answer =  new int[que.size()];
-    	// 7 3 9
-    	int com = 0;	//비교 값
-    	boolean isTrue = true;
-    	int add = 0;	
-    	for(int result : que) {
-    		if(isTrue) {
-    			com = result;
-    			isTrue = false;
-    			add++;
-    		}
-    		if(com> result) {
-    			add++;
-    		} 
-    		System.out.println(result);
-    	}
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> que = new LinkedList<>();
+        int time;
+        for(int i=0; i<progresses.length; i++) {
+            int cnt=1; 
+            time = 100 - progresses[i];
+            while(time > 0) {
+                time -= speeds[i];
+                cnt++;
+            }
+            que.add(cnt-1);
+        }
+         List<Integer> result = new ArrayList<Integer>();
+         int standard = que.poll();
+         int add = 1;
+         while(!que.isEmpty()) {
+             int num = que.poll();
+             if(standard >= num) {
+                 add++;
+             }else {
+                 result.add(add);
+                 add = 1;
+                 standard = num;
+             }
+         }
+         result.add(add);
+         int[] answer =  new int[result.size()];
+         for(int i = 0; i < answer.length; i++){
+             answer[i] = result.get(i);
+         }
         return answer;
     }
 }
-
-
