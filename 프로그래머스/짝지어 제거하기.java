65f9757f1b1ee3a9a5a90c91a7fd8ed584150703
kangilbin <kib3388@naver.com>
2022-07-31 23:01:03 +0900

    짝지어 제거하기

---
+++ 프로그래머스/짝지어 제거하기.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+class Solution
+{
+    public int solution(String s)
+    {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (stack.isEmpty())
+				stack.push(s.charAt(i));
+			else {
+				if (stack.peek().equals(s.charAt(i))) {
+					stack.pop();
+
+				} else {
+					stack.push(s.charAt(i));
+				}
+			}
+		}
+		return stack.size() > 0 ? 0 : 1;
+	}
+}
